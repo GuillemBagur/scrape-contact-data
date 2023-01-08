af02395642360e244c14d0b482718bf5dd49debd
Guillem Bagur <guillembagurmoll@gmail.com>
2023-01-08 12:39:27 +0100

    Add CORS Middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const { getPossibleCustomers } = require("./js/scrape");
 
 const app = express();
 app.use(express.static(path.join(__dirname, "../src")));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.listen(3000, () => console.log("listening on port 3000"));
 
